9283b12d7ffb62926b23df47d5e9c60f2f3002d3
Athuro83 <arthu@live.fr>
2018-04-01 20:57:36 +0200

    Synchro du main avec template prof

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 /// N'ajouter aucun autre code dans ce fichier. 
 int main(int argc, char *argv[])
 {
-	char req[]="GET / HTTP/1.0\r\nHost: www.google.com\r\nTransfer-Encoding: gzip\r\n\r\n";
+	char req[]="GET http://www.google.com/ HTTP/1.0\r\nHost: www.google.com\r\nTransfer-Encoding: gzip;\ntar\r\n\r\n";
 	if (argc != 2 ) { printf("usage: %s <rulename>\n",argv[0]); return 0; }
 	if (parseur(req,strlen(req))) {
 		_Token *r,*tok; 
